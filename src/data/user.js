1b9574d43ce1fdeae35ac22f2138bb02b22dd002
jlikithsai <117892485+jlikithsai@users.noreply.github.com>
2023-07-08 17:38:59 +0530

    Adding Source files

---
+++ src/data/user.js
@@ -0,0 +1,68 @@
+const INFO = {
+	main: {
+		title: "Likith's Portfolio in Reactjs",
+		name: "Likith Sai",
+		email: "jonnals22@iitk.ac.in",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/jlikithsai",
+		linkedin: "https://www.linkedin.com/in/likith-sai-jonna-6102b7257/",
+		instagram: "https://www.instagram.com/likith.sai_1303/",
+		stackoverflow: "https://stackoverflow.com",
+		facebook: "https://www.facebook.com/profile.php?id=100085892250701",
+	},
+
+	homepage: {
+		title: "Undergrad @IITK,Amatuer Programmer,Electronics Enthusiast",
+		description:
+			"Hi,This is Likith sai Jonna.I am an amatuer programmer, I love building quality code and cool webpages and also have decent exposure towards Data Structures and Algorithms and love to use them in making really efiicient codes ",
+	},
+
+	about: {
+		title: "I’m Likith Sai. I live in India, where I pursue intrests",
+		description:
+			"I've just begun working on opensource projects and am really entusiastic to explore the world of opensource programming and programmming in general. Neverthless, I have some projects at my institute which i can anyways flex upon and i am looking forward to work on many other such projects. I have expertise in C++,C and python and I am gaing some handson experience on HTML,CSS,JavaScript and its frameworks Glad that you've read till here looking forward to colloborate",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Building a Battlebot",
+			description:
+				"Worked with team of 30 members to develop a 60 kg robot to fight in an arena against another bot of 60 kg. We also participated in Battlebot International",
+			logo: "https://on-camera-audiences.com/shows/BattleBots/img",
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1VK-VAgptRPjDevk-wD2GTaPJ5ajkMnwl?usp=sharing",
+		},
+
+		{
+			title: "Mathemetical Model: Machine Learning",
+			description:
+				"Got a brief understanding of basic concepts of Machine Learning and the Mathematics behind built a few models using numpy library of python and few other extensions you can find teh assignments in the projects link below",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1iAN_BLmCAbB9JbwX1rTibwICk_hJXE1t?usp=sharing",
+		},
+
+		{
+			title: "Basic Portfolio website in HTML,CSS and javascript",
+			description:
+				"Built my own personal portfolio from website using basics of HTML,CSS and Java Script you will be redirectd to the website by clicking on the link below",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link:"https://jlikithsai.github.io/MyWebpage/",
+		},
+
+		
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		document.title = `Projects | ${INFO.main.title}`;
+		window.scrollTo(0, 0);
+	}, []);
+
+	return (
+		<React.Fragment>
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Things I’ve made trying to put my dent in the
+							universe.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+							I've begun working on a variety of projects and am ethusiastic in building many 
+							more i feel that projects are essential to have a better and more practical 
+							understanding on everything we learn and can cionfidently say that all the practical 
+							knowledge i have on programming has come through one of these.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
